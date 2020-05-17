ffe0f274729d1c56185b3542d9fc7ec930cce631
w-flowers <wig.flowers@gmail.com>
2020-05-17 17:33:38 +1000

    Completed implementation of postorder tree traversal.
    - Completed implementation of postorder tree traversal
    - Added a data_structures file, which is designed to allow the programmer to access basic data structures
    - In said data structures file, modified each structure to take void pointers, thus allowing them to be used in algorithms with any type

--- data_structures.c
+++ data_structures.c
@@ -1,22 +1,27 @@
 #include <stdlib.h>
 
+
 /* Author: William Flowers
 //
 // Purpose: This library exists to provide some basic data structures and operations pertaining to 
-// them. Defined herein are trees, linked lists, stacks and queues
+// them. Defined herein are trees, linked lists, stacks and queues. The main intended usage of this
+// module is to demonstrate example algorithms for the various data structures defined herein. This
+// module is NOT intended for usage in production code.
 //
-// Usage: All of these data structures take an integer "key". One possible way of using these more
-// generally would be to store your data in an array, and use these keys to access those elements.
+// Usage: All of these data structures take an void pointer "key". It is the responsibility of the
+// caller to ensure that the data entered and returned is of the correct type.
 */
 
+
+
 struct t_node {
-	int key;
+	void *key;
 	struct t_node *l;
 	struct t_node *r;
 };
 
 struct ll_node {
-	int key;
+	void *key;
 	struct ll_node *next;
 };
 
@@ -27,44 +32,58 @@ struct queue {
 
 struct stack {
 	struct ll_node *top;
-}
+};
 
 //Creates an empty queue, with front and back initialised as NULL
-struct queue *queue_init(int k);
+struct queue *queue_init();
 
 //Adds an element to the back of the passed queue. If queue is empty, element becomes front and back
-int put(int k, struct queue *q);
+int put(struct queue *q, void *k);
 
 //Gets an element from the front of a queue. If element is the last element of the queue, the back
 //of the queue is set to NULL as well
 //If not called in conjunction with put, may result in freeing of non-allocated memory
-int get(struct queue *q);
+void *get(struct queue *q);
 
 //returns 0 if the queue has elements, or 1 if it does not
 int queueempty(struct queue *q);
 
+//De-initialises the queue and frees all allocated memory
+int destroy_queue(struct queue *q);
+
 //creates an empty stack, with top initialised as NULL
-struct stack *stack_init(int k);
+struct stack *stack_init();
 
 //Adds an element to the top of the stack.
-int push(struct stack *s, int k);
+int push(struct stack *s, void *k);
 
-//Gets an element from the top of the stack. If stack is empty, returns 0
+//Gets an element from the top of the stack. If stack is empty, returns NULL
 //If called not in conjunction with push, may result in freeing of non-allocated memory
-int pop(struct stack *s);
+void *pop(struct stack *s);
+
+
+//Looks at top element of stack
+void *peek(struct stack *s);
 
 //returns 0 if the stack has elements, else returns 1
 int stackempty(struct stack *s);
 
+int destroy_stack(struct stack *s);
+
 //A function that takes an integer key k and initialises a tree with a root node with a value of k
-struct t_node *tree_init(int k);
+struct t_node *tree_init(void *k);
 
 //A function that adds a node to the tree, going through the tree level by level, left to right, until
 //it finds a NULL, at which point it adds the integer k, returns 0 for success, 1 for malloc failure
 //If passed a NULL pointer, makes the new element the root node
-int t_add_node(int k, struct node *tree_p);
+int t_add_node(void *k, struct t_node *tree_p);
 
-struct queue *queue_init(int k){
+//destroy tree moves through the tree in level order and deallocates the memory for the structure.
+//This must be called if a tree is created and built using "tree_init" and "t_add_node".
+//Do not call this on any tree allocated manually on the stack.
+int destroy_tree(struct t_node *tree_p);
+
+struct queue *queue_init(){
 	struct queue *p = malloc(sizeof(struct queue));
 	if(p){
 		p->front = NULL;
@@ -74,7 +93,7 @@ struct queue *queue_init(int k){
 	else return NULL;
 }
 
-int put(int k, struct queue *q){
+int put(struct queue *q, void *k){
 	struct ll_node *new_ele = malloc(sizeof(struct ll_node));
 	if(!new_ele) return 1;
 	new_ele->key = k;
@@ -90,9 +109,9 @@ int put(int k, struct queue *q){
 	return 0;
 }
 
-int get(struct queue *q){
-	if(q->front == NULL) return 0;
-	int k = q->front->key;
+void *get(struct queue *q){
+	if(q->front == NULL) return NULL;
+	void *k = q->front->key;
 	struct ll_node *dlt = q->front;
 	q->front = q->front->next;
 	if(q->front == NULL) q->back = NULL;
@@ -105,8 +124,18 @@ int queueempty(struct queue *q){
 	else return 0;
 }
 
-struct stack *stack_init(int k){
-	struct queue *s = malloc(sizeof(struct stack));
+int destroy_queue(struct queue *q){
+	for(struct ll_node *dummy = q->front; dummy != NULL;){
+		struct ll_node *dlt = dummy;
+		dummy = dummy->next;
+		free(dlt);
+	}
+	free(q);
+	return 0;
+}
+
+struct stack *stack_init(){
+	struct stack *s = malloc(sizeof(struct stack));
 	if(s){
 		s->top = NULL;
 		return s;
@@ -114,7 +143,7 @@ struct stack *stack_init(int k){
 	else return NULL;
 }
 
-int push(struct stack *s, int k){
+int push(struct stack *s, void *k){
 	struct ll_node *new_ele = malloc(sizeof(struct ll_node));
 	if(!new_ele) return 1;
 	new_ele->key = k;
@@ -123,13 +152,20 @@ int push(struct stack *s, int k){
 	return 0;
 }
 
-int pop(struct stack *s){
-	if(s->top == NULL) return 0;
-	int k = s->top->key;
+void *pop(struct stack *s){
+	if(s->top == NULL) return NULL;
+	void *k = s->top->key;
 	struct ll_node *dlt = s->top;
 	s->top = s->top->next;
 	free(dlt);
 	return k;
+}
+
+void *peek(struct stack *s){
+	if(!stackempty(s)){
+		return s->top->key;
+	}
+	else return 0;
 }
 
 int stackempty(struct stack *s){
@@ -137,28 +173,74 @@ int stackempty(struct stack *s){
 	else return 0;
 }
 
-struct t_node *tree_init(int k){
+int destroy_stack(struct stack *s){
+	for(struct ll_node *dummy = s->top; dummy != NULL;){
+		struct ll_node *dlt = dummy;
+		dummy = dummy->next;
+		free(dlt);
+	}
+	free(s);
+	return 0;
+}
+
+struct t_node *tree_init(void *k){
 	struct t_node *p = malloc(sizeof(struct t_node));
 	if(p){
 		p->l = NULL;
 		p->r = NULL;
-		key = k;
+		p->key = k;
 		return p;
 	}
 	else return NULL;
 }
 
-int add_node(int k, struct t_node *tree_p){
-	struct t_node *p = malloc(sizeof(struct node));
-	if(p){
-		p->l = NULL;
-		p->r = NULL;
-		p->key = k;
-		while(tree_p != NULL){
-			if(tree_p->l == NULL) tree_p->l = p;
-			if(tree_p->r == )
+int add_node(void *k, struct t_node *tree_p){
+	struct t_node *p = malloc(sizeof(struct t_node));
+	struct t_node *t = tree_p;
+	if(!p) return 1;
+	struct queue *q = queue_init();
+	if(!q) return 1;
+	put(q, t);
+	p->l = NULL;
+	p->r = NULL;
+	p->key = k;
+	while(!queueempty(q)){
+		if(t->l == NULL){
+			t->l = p;
+			break;
 		}
-		else tree_p = p;
+		else{
+			put(q, t->l);
+		}
+		if(t->r == NULL){
+			t->r = p;
+			break;
+		}
+		else{
+			put(q, t->r);
+		}
+		t = get(q);
 	}
-	else return 1;
+	destroy_queue(q);
+	return 0;
+}
+
+int destroy_tree(struct t_node *tree_p){
+	if(tree_p == NULL) return 0;
+	struct queue *q = queue_init();
+	if(!q) return 1;
+	struct t_node *t = tree_p;
+	put(q, t);
+	while(!queueempty(q)){
+		t = get(q);
+		if(t->l != NULL){
+			put(q, t->l);
+		}
+		if(t->r != NULL){
+			put(q, t->r);
+		}
+		free(t);
+	}
+	destroy_queue(q);
+	return 0;
 }
--- postorder.c
+++ postorder.c
@@ -1,2 +1,33 @@
 #include <stdio.h>
+#include "data_structures.c"
 
+int main(){
+	int array[1024];
+	for(int i = 0; i<1024; i++) array[i] = i+1;
+	struct t_node *root = tree_init(&array[0]);
+	for(int i = 2; i < 1024; i++) add_node(&array[i], root);
+	struct t_node *t = root;
+	struct stack *s = stack_init();
+	do{
+		while(t != NULL){
+			if(t->r != NULL){
+				push(s, t->r);
+			}
+			push(s, t);
+			t = t->l;
+		}
+		t = pop(s);
+		if(t->r == peek(s)){
+			pop(s);
+			push(s, t);
+			t = t->r;
+		}
+		else{
+			printf("%d\n", *(int *)(t->key));
+			t = NULL;
+		}
+	}while(!stackempty(s));
+	destroy_tree(root);
+	destroy_stack(s);
+	return 0;
+}
