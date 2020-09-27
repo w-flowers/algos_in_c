bed0a24721303b70195ea26e74b5346d141ab892
w-flowers <wig.flowers@gmail.com>
2020-09-27 17:13:50 +1000

    Moved non-recursive algos to qsort_w_insort.c
    - Moved nrc algos so that they could be benchmarked on the same arrays
    as the recursive code
    - Experimented with different values for array size, insert sort threshold
    and size of random numbers
    - Performance varies hugely with properties of arrays to be sorted -
    more investigation required

--- qs_w_is_nr_v2.c
+++ qs_w_is_nr_v2.c
@@ -1,17 +1,18 @@
 /***********************************
  * William Flowers
  *
- * Solution to Problem 1 Chapter 9 of Algorithms in C
+ * Solution to Problem 2 Chapter 9 of Algorithms in C
  *
  * Implementation of quicksort which switches to insert sort for
- * files below a ceratin size
+ * files below a ceratin size - NOTE: non-recursive quicksort
  * *******************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <time.h>
+#include "data_structures_int.c"
 
-#define SIZE 50000
+#define SIZE 500000
 
 #define THRSH 30 
 
@@ -49,71 +50,11 @@ int insert_sort_range(int arr[], int l, int r)
 
 int qsort_w_insort(int arr[], int l, int r)
 {
-   int i = l, j = r;
-
-   int dmy = 0;
-
-   if( r - l < THRSH ) insert_sort_range( arr, l, r );
-
-   else
-   {
-      while(1)
-      {
-         while( arr[++i] < arr[r] );
-
-         while( arr[--j] > arr[r] );
-
-         if( i >= j ) break;
-
-         dmy = arr[i], arr[i] = arr[j], arr[j] = dmy;
-      }
-      dmy = arr[i], arr[i] = arr[r], arr[r] = dmy;
-
-      qsort_w_insort( arr, l, i - 1 );
-
-      qsort_w_insort( arr, i, r );
-   }
-
-   return 0;
-
-}
-
-int myqsort(int arr[], int l, int r)
-{
-   int i = l, j = r;
-
-   int dmy = 0;
-
-   if(r - l > 1)
-   {
-      while(1)
-      {
-         while( arr[++i] < arr[r] );
-
-         while( arr[--j] > arr[r] );
-
-         if( i >= j ) break;
-
-         dmy = arr[i], arr[i] = arr[j], arr[j] = dmy;
-      }
-      dmy = arr[i], arr[i] = arr[r], arr[r] = dmy;
-
-      myqsort( arr, l, i - 1 );
-
-      myqsort( arr, i, r );
-   }
-
-   return 0;
-
-}
-
-int qsort_w_insort_nrc(int arr[], int l, int r)
-{
    int i, j, k, m;
 
    int dmy = 0;
 
-   int stack[SIZE/20];
+   int stack[SIZE/2];
 
    int top = 0;
 
@@ -161,13 +102,13 @@ int qsort_w_insort_nrc(int arr[], int l, int r)
 
 }
 
-int myqsort_nrc(int arr[], int l, int r)
+int myqsort(int arr[], int l, int r)
 {
    int i, j, k, m;
 
    int dmy = 0;
 
-   int stack[SIZE/20];
+   int stack[SIZE/2];
 
    int top = 0;
 
@@ -207,11 +148,13 @@ int myqsort_nrc(int arr[], int l, int r)
    return 0;
 }
 
+
+
 int main()
 {
-   int array[SIZE + 1], array1[SIZE + 1], array2[SIZE + 1], array3[SIZE + 1];
+   int array[SIZE + 1], array1[SIZE + 1];
 
-   array[0] = INT_MIN;   array1[0] = INT_MIN; array2[0] = INT_MIN; array3[0] = INT_MIN;
+   array[0] = INT_MIN;   array1[0] = INT_MIN;
 
    srand((int) time(NULL));
 
@@ -220,22 +163,16 @@ int main()
 
       array1[i] = array[i];
 
-      array2[i] = array[i];
-
-      array3[i] = array[i];
-
       //printf("%d ", array[i]);
    }
    
    printf("\n");
 
-   clock_t start, end;
-  
-   start = clock();
+   clock_t start = clock();
 
    qsort_w_insort(array, 0, SIZE);
 
-   end = clock();
+   clock_t end = clock();
 
    for(int i = 1; i < SIZE; i++) if(array[i] > array[i+1])
    {
@@ -262,36 +199,7 @@ int main()
    }
 
    printf("qs: %ld\n", (end - start));
-  
-   start = clock();
 
-   qsort_w_insort_nrc(array2, 0, SIZE);
-
-   end = clock();
-
-   for(int i = 1; i < SIZE; i++) if(array2[i] > array2[i+1])
-   {
-      printf("qwi_nrc: Array not sorted!!\n");
-
-      break;
-   }
-
-   printf("qwi_nrc: %ld\n", (end - start));
-
-   start = clock();
-
-   myqsort_nrc(array3, 0, SIZE);
-
-   end = clock();
-
-   for(int i = 1; i < SIZE; i++) if(array3[i] > array3[i+1])
-   {
-      printf("qs_nrc: Array not sorted!!\n");
-
-      break;
-   }
-
-   printf("qs_nrc: %ld\n", (end - start));
 
    return 0;
 }
